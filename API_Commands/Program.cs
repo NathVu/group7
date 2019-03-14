58cb5f1ab6faf645a6485997f1f197030c52d5b2
NathVu <31414875+NathVu@users.noreply.github.com>
2019-03-14 19:45:28 -0400

    Readability and functionality update for API Query
    RE-organized code for easier readability including making a class to handle API calls and setting the Date and Time to the systems date and time (working on fixing a bug here) instead of manually updating it daily. Also removed (almost) all references using dynamic typing and replaced them with statically typed variables for readability

--- API_Commands/Program.cs
+++ API_Commands/Program.cs
@@ -11,6 +11,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            ManageDB test = new ManageDB();
+            var results = test.LoadDB();
+        }
+
+    }
+
+    /*
+     * Class for managing the Database
+     * created for code clean-up and easier readability
+     */ 
+    class ManageDB
+    {
+        public ManageDB() {}
+
+
+        public IEnumerable<Dictionary<string,object>> LoadDB()
+        {
 
             /*
              * Assisted by code from https://dev.socrata.com/foundry/data.cityofnewyork.us/fhrw-4uyv
@@ -19,7 +36,7 @@ namespace ConsoleApp1
              * queried DB - https://data.cityofnewyork.us
              * our API KEY - PVGjhHLj8Svy7Ryz0uJgW9IBh
              */
-            var client = new SodaClient("https://data.cityofnewyork.us", "PVGjhHLj8Svy7Ryz0uJgW9IBh");
+            SODA.SodaClient client = new SodaClient("https://data.cityofnewyork.us", "PVGjhHLj8Svy7Ryz0uJgW9IBh");
 
             /*
              * IMPORTANT: Since the website has been updated, the .NET library has been updated
@@ -27,8 +44,55 @@ namespace ConsoleApp1
              * <Dictionary<string,object>> (the dictionary collection found in System.Collections.Generic
              * or a user defined class <MyClass>
              */
-            var dataset = client.GetResource<Dictionary<string,object>>("fhrw-4uyv");
-            //var dataset = client.GetResource<Json311>("fhrw-4uyv");
+            SODA.Resource<Dictionary<string,object>> dataset = client.GetResource<Dictionary<string, object>>("fhrw-4uyv");
+            //SODA.Resource<Json311> dataset = client.GetResource<Json311>("fhrw-4uyv"); //testing our serializable json class
+
+
+            ManageDB test = new ManageDB();
+            SODA.SoqlQuery soql = test.GetQueryDate();
+
+            /*
+             * Results saves the data as System.Collections.Generic.List according to .GetType()
+             * and saved as IEnumerable with they dype Dictionary<string,object> according to visual studio
+             * 
+             * this field remains a var type as we are working on changing it to our user defined class
+             */
+            var results = dataset.Query<Dictionary<string, object>>(soql);
+
+            /*
+             * there are elements in the list, now just need to access them
+             * test to check if there really are elements in the list
+             */
+            int SizeOfList;
+            test.TestIEnum(ref results, out SizeOfList);
+            Console.WriteLine(SizeOfList);
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+            return results;
+        }
+
+
+        /*
+         * For Better readablity and enables the program to more easily make changes to the query
+         * As we can later write another function instead of just continually changing this on         
+         * this also allows a neater place to stick the DateTime variable that will automaticall set the date
+         */ 
+        public SODA.SoqlQuery GetQueryDate()
+        {
+            /*
+             * Added so that we do not need to keep manually changing the date stamp in the query 
+             * today is the time in nanoseconds, date translates it to a usable string in the correct format
+             * for the query
+             */
+            DateTime today = DateTime.Today;
+            String date = today.ToString("yyyy-mm-dd");
+
+            /*
+             * Working on resolving issue where DateTime returns the month as 00
+             */ 
+            Console.WriteLine(date);
 
             /*
              * IMPORTANT: this library has a dependancy, install in NuGet Install-Package System.Security.Permissions
@@ -37,48 +101,26 @@ namespace ConsoleApp1
              * Timestamp (or other literal) must be in quotes or else it gets treated as a variable which causes the
              * SoqlQuery to throw a bad request exception
              */
-            var soql = new SoqlQuery().Select("*").Limit(50);//.Where("created_date > \"2019-03-12\" ");
+            SODA.SoqlQuery soql = new SoqlQuery().Select("*").Limit(10);
 
             /*
-             * Results saves the data as System.Collections.Generic.List according to .GetType()
-             * and saved as IEnumerable according to visual studio
+             * Having an issue with the query we want, using the Limit query (above) to test functionality of the code
              */ 
-            var results = dataset.Query<Dictionary<string, object>>(soql);
-            //var results = dataset.Query<Json311>(soql);
+            //SODA.SoqlQuery soql = new SoqlQuery().Select("*").Where("created_date > \"" + date + "T00:00:00.000\" ");
 
-            Console.WriteLine(results.GetType());
 
-            /*
-             * Currently throws error: sequence contains no elements
-             */
-            int SizeOfList;
-            void Start(){
-                SizeOfList = results.Count();
-            }
-            Start();
-            Console.WriteLine(SizeOfList);
-            Console.ReadKey();
-
-           /* foreach (Dictionary<string, object> kvp in results)
-            {
-                Console.WriteLine("key is {0}, Value is {1}", kvp.Keys, kvp.Values);
-            }
-            Console.ReadKey();
-            */
-
+            return soql;
         }
 
         /*
-         * For eventual code cleanup - will move the query code from main to here
+         * Internal test to make sure the query returned data 
          */ 
-         /*
-        public void loadDB()
+        public void TestIEnum(ref IEnumerable<Dictionary<string, object>> testDB, out int SizeOfList)
         {
-            var client = new SodaClient("https://data.cityofnewyork.us", "PVGjhHLj8Svy7Ryz0uJgW9IBh");
-
+            SizeOfList = testDB.Count();
         }
-        */
     }
+
 
     class Json311
     {
