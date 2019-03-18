4690e0e15b1f65ef27abc0a64783fc030d5960d5
NathVu <31414875+NathVu@users.noreply.github.com>
2019-03-18 11:27:52 -0400

    Fixed API Query
    Resolved issues in the API class. Updated comments for readablilty

--- API_Commands/Program.cs
+++ API_Commands/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Permissions;
-using System.Text;
 using SODA;
 
 namespace ConsoleApp1
@@ -17,52 +15,69 @@ namespace ConsoleApp1
 
     }
 
-    /*
-     * Class for managing the Database
-     * created for code clean-up and easier readability
-     */ 
+    
+     /// <summary> Class for managing the Database
+     /// created for code clean-up and easier readability 
+     /// </summary>
     class ManageDB
     {
         public ManageDB() {}
 
 
+        /// <summary>
+        /// Assisted by code from https://dev.socrata.com/foundry/data.cityofnewyork.us/fhrw-4uyv
+        /// queried DB - https://data.cityofnewyork.us
+        /// API KEY - PVGjhHLj8Svy7Ryz0uJgW9IBh
+        /// loadDB connects to the database, sends the query and then returns the data
+        /// </summary>
+        /// <returns>Returns the dataset of the query to main</returns>
         public IEnumerable<Dictionary<string,object>> LoadDB()
         {
 
-            /*
-             * Assisted by code from https://dev.socrata.com/foundry/data.cityofnewyork.us/fhrw-4uyv
-             * using the SODA library install command with NuGet in Visual Studio- Install-Package CSM.SodaDotNet
-             * The goal is to use the Npgsql and the Soda.NET library to download the data automatically and load it into our PostgresDB
-             * queried DB - https://data.cityofnewyork.us
-             * our API KEY - PVGjhHLj8Svy7Ryz0uJgW9IBh
-             */
+            
+            ///<remarks>
+            ///This requires the SODA library 
+            ///It can be installed from NuGet in Visual studio using 
+            ///Install-Package CSM.SodaDotNet
+            ///</remarks>
+             
             SODA.SodaClient client = new SodaClient("https://data.cityofnewyork.us", "PVGjhHLj8Svy7Ryz0uJgW9IBh");
 
-            /*
-             * IMPORTANT: Since the website has been updated, the .NET library has been updated
-             * and SodaClient.GetResource no longer allows for generic typing, it requires either
-             * <Dictionary<string,object>> (the dictionary collection found in System.Collections.Generic
-             * or a user defined class <MyClass>
-             */
+
+             ///<remarks>
+             /// The documentation on the web is outdated.
+             /// The .NET library has been updated to no longer allow generic typing. 
+             /// You must use either Dictionary(String,Object) - use <> but not allowed in XML comments
+             /// OR a user-defined json serializable class
+             ///</remarks>
             SODA.Resource<Dictionary<string,object>> dataset = client.GetResource<Dictionary<string, object>>("fhrw-4uyv");
+
+            ///<remarks>
+            ///This is a test for our user defined serializable class
+            ///It may be implemented in later versions 
+            ///</remarks>
             //SODA.Resource<Json311> dataset = client.GetResource<Json311>("fhrw-4uyv"); //testing our serializable json class
 
-
+            ///<summary>
+            /// instantiate our object and get our query from GetQueryDate()
+            /// </summary>
             ManageDB test = new ManageDB();
             SODA.SoqlQuery soql = test.GetQueryDate();
 
-            /*
-             * Results saves the data as System.Collections.Generic.List according to .GetType()
-             * and saved as IEnumerable with they dype Dictionary<string,object> according to visual studio
-             * 
-             * this field remains a var type as we are working on changing it to our user defined class
-             */
+
+            ///<summary>
+            /// Query sends our query to our pre-defined location and returns an IEnumerable which we assign to results
+            /// results now contains the results of our query
+            /// 
+            /// This field for now remains dynamically typed in case we decide to change to our user defined class
+            /// </summary>
             var results = dataset.Query<Dictionary<string, object>>(soql);
 
-            /*
-             * there are elements in the list, now just need to access them
-             * test to check if there really are elements in the list
-             */
+
+
+            ///<summary>
+            /// Testing to make sure that our query returned results
+            ///</summary>
             int SizeOfList;
             test.TestIEnum(ref results, out SizeOfList);
             Console.WriteLine(SizeOfList);
@@ -74,56 +89,97 @@ namespace ConsoleApp1
         }
 
 
-        /*
-         * For Better readablity and enables the program to more easily make changes to the query
-         * As we can later write another function instead of just continually changing this on         
-         * this also allows a neater place to stick the DateTime variable that will automaticall set the date
-         */ 
+
+
+        /// <summary>
+        /// This function was written to increase readability of the code
+        /// It gets the current data (and yesterdays date) so that the code does not need to be updated every day
+        /// and composes a query object (SODA.SoqlQuery) to reurn to LoadDB to query the dataset
+        /// Utilizes the DateTime type to get todays date
+        /// </summary>
+        /// <returns>our Query in their defined type - SODA.SoqlQuery </returns>
         public SODA.SoqlQuery GetQueryDate()
         {
-            /*
-             * Added so that we do not need to keep manually changing the date stamp in the query 
-             * today is the time in nanoseconds, date translates it to a usable string in the correct format
-             * for the query
-             */
+            
             DateTime today = DateTime.Today;
-            String date = today.ToString("yyyy-mm-dd");
+            String year = today.Year.ToString();
 
-            /*
-             * Working on resolving issue where DateTime returns the month as 00
-             */ 
+
+            ///<remarks>
+            /// Their query requires typing with a 0 in front of the month so we 
+            /// check the formatting and add a 0 if necessary
+            /// </remarks>
+            String month;
+            if (today.Month / 10 != 0)
+            {
+                month = today.Month.ToString();
+            }
+            else
+            {
+                month = "0" + today.Month.ToString();
+            }
+            String day = today.Day.ToString();
+            String yday = (today.Day - 1).ToString();
+        
+
+            ///<remarks>
+            ///The date field in the Query needs to be of type
+            ///yyyy-mm-dd + T + hh:mm:ss.nnn (hours, minutes, seconds and nanoseconds.
+            ///We format the data here to be in the correct type as the ToString method provided by 
+            ///the DateTime class does not allow for formatting in this manner
+            /// </remarks>
+            String date= "\"" + year + "-" + month + "-" + day + "T00:00:00.000\"";
+            String ydate = "\"" + year + "-" + month + "-" + yday + "T00:00:00.000\"";
+ 
+            ///<remarks>
+            /// just a quick test to make sure the date is in the format we want
+            /// </remarks>
             Console.WriteLine(date);
 
-            /*
-             * IMPORTANT: this library has a dependancy, install in NuGet Install-Package System.Security.Permissions
-             * This line will otherwise throw an error
-             * usage: Where("created_date > \"timestamp\" "); 
-             * Timestamp (or other literal) must be in quotes or else it gets treated as a variable which causes the
-             * SoqlQuery to throw a bad request exception
-             */
-            SODA.SoqlQuery soql = new SoqlQuery().Select("*").Limit(10);
+            ///<remarks>
+            /// A test case just to test that the connection is working
+            /// will be removed in later revisions
+            /// </remarks>
+            //SODA.SoqlQuery soql = new SoqlQuery().Select("*").Limit(10);
 
-            /*
-             * Having an issue with the query we want, using the Limit query (above) to test functionality of the code
-             */ 
-            //SODA.SoqlQuery soql = new SoqlQuery().Select("*").Where("created_date > \"" + date + "T00:00:00.000\" ");
+           ///<remarks>
+           /// for increased readability and easier formatting
+           /// </remarks>
+            string cdate = "created_date";
+
+            ///<remarks>
+            /// Usage of creating a new SoqlQuery - 
+            /// Note: TimeStamp (or other literal) must be in quotes or else it gets treated like a variable which will throw an error
+            /// Field + "function" + "comparison type"
+            /// Select can be used to select specific fields but we want all data associated with the calls
+            /// </remarks>
+            SODA.SoqlQuery soql = new SoqlQuery().Select("*").Where(cdate + ">" + ydate);
 
 
             return soql;
         }
+        
 
-        /*
-         * Internal test to make sure the query returned data 
-         */ 
+        /// <summary>
+        /// Internal Test to make sure the query is returning data and not an empty set
+        /// </summary>
+        /// <param name="testDB"> The database we want to test</param>
+        /// <param name="SizeOfList"> An out paramted - returns the number of data items in the database</param>
         public void TestIEnum(ref IEnumerable<Dictionary<string, object>> testDB, out int SizeOfList)
         {
             SizeOfList = testDB.Count();
         }
     }
 
-
+    
+     /// <summary>
+     /// Template for our seriablizable class to potentially replace Dictionary(string, object) (same <> notation, see reason above)
+     /// Will be removed if Dictionary proves to be more useful and useable
+     /// </summary>
     class Json311
     {
+        public string unique_key { get; set; }
+        public string created_date { get; set; }
         public string closed_date { get; set;}
         public string agency { get; set; }
         public string complaint_type { get; set; }
