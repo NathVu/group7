1a49bc57c13a04e432454d7111f1678ef9902bee
NathVu <31414875+NathVu@users.noreply.github.com>
2019-03-24 15:38:15 -0400

    Restructuring and Storage Update
    Removed old powershell query, moved Json311 (our user-created class) into its own file for readability increase. Parsing and saving Data in our user created format

--- API_Commands/Program.cs
+++ API_Commands/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 using SODA;
+using Json311;
 
 namespace ConsoleApp1
 {
@@ -9,31 +11,95 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ManageDB test = new ManageDB();
-            var results = test.LoadDB();
-
-
-            //IEnumerator<Json311> index = results.GetEnumerator();
-
-
-            var rarr = results.ToArray();
-            var val = rarr[0];//.ToString();
-            Console.WriteLine(val.Values.Count());
+            DataFormat test = new DataFormat();
+            Dictionary<string, object>[] rarr = test.getData();
+            test.parseData(rarr);
             Console.ReadKey();
-       
+
+            /*
+             * Temp Code: holding here until incorporated into our program 
+             * var val = rarr[0];
+             * Console.WriteLine(val.Values.Count());
+             * foreach (KeyValuePair<string, object> iterate in val)
+             * {
+             *   Console.WriteLine(iterate.Key + " " + iterate.Value.GetType());
+             * }
+             * Console.ReadKey();
+             * 
+             */
 
         }
 
     }
 
-    
-     /// <summary> Class for managing the Database
-     /// created for code clean-up and easier readability 
-     /// </summary>
+
+
+
+    /// <summary>
+    ///  A class to take the data recieved from the API Query
+    ///  And format it usiing our user defined class to 
+    /// </summary>
+    class DataFormat
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataset"> recieves out dataset, formatted in getData into an array of Dictionary objects</param>
+        /// <returns>The data parsed into our user created class</returns>
+        public List<Json311.Json311> parseData(Dictionary<string, object>[] dataset)
+        {
+            List<Json311.Json311> dataList = new List<Json311.Json311>();
+            
+
+            for (int i = 0; i < dataset.Length; i++)
+            {
+                Json311.Json311 dItem = new Json311.Json311(dataset[i]);
+                //if(dItem.Created_date > )
+                dataList.Add(dItem);
+                Console.WriteLine("working: " + i);
+            }
+
+            return dataList;
+        }
+
+        /// <summary>
+        /// Manages our Database function call and gets back the Data as they return us
+        /// And converts it to an array which we return
+        /// </summary>
+        /// <returns>A Dictionary Array which we can read through</returns>
+        public Dictionary<string, object>[] getData()
+        {
+            ManageDB test = new ManageDB();
+            IEnumerable<Dictionary<string, object>> results = test.LoadDB();
+
+            /// <remarks>
+            /// Allows us to read the data
+            /// We convert the dara into an array so it is not in an interface and we are able 
+            /// to read the data that our query returned 
+            /// </remarks>
+            Dictionary<string, object>[] results_arr = results.ToArray();
+            Dictionary<string, object> val = results_arr[0];
+            Console.WriteLine(val.Values.Count());
+            foreach (KeyValuePair<string, object> iterate in val)
+            {
+                Console.WriteLine(iterate.Key.ToString() + " " + iterate.Value.GetType() + " " + iterate.Value);
+            }
+            Console.ReadKey();
+            return results_arr;
+        }
+    }
+
+
+
+
+
+    /// <summary> Class for managing the Database
+    /// created for code clean-up and easier readability 
+    /// </summary>
     class ManageDB
     {
-        public ManageDB() {}
-
+        public ManageDB() { }
 
         /// <summary>
         /// Assisted by code from https://dev.socrata.com/foundry/data.cityofnewyork.us/fhrw-4uyv
@@ -45,60 +111,43 @@ namespace ConsoleApp1
         public IEnumerable<Dictionary<string, object>> LoadDB()
         {
 
-            
-            ///<remarks>
-            ///This requires the SODA library 
-            ///It can be installed from NuGet in Visual studio using 
-            ///Install-Package CSM.SodaDotNet
-            ///</remarks>
-             
+            /// <remarks>
+            /// This requires the SODA library 
+            /// It can be installed from NuGet in Visual studio using 
+            /// Install-Package CSM.SodaDotNet
+            /// </remarks>>   
             SODA.SodaClient client = new SodaClient("https://data.cityofnewyork.us", "PVGjhHLj8Svy7Ryz0uJgW9IBh");
 
-
-             ///<remarks>
-             /// The documentation on the web is outdated.
-             /// The .NET library has been updated to no longer allow generic typing. 
-             /// You must use either Dictionary(String,Object) - use <> but not allowed in XML comments
-             /// OR a user-defined json serializable class
-             ///</remarks>
+            /// <remarks>
+            /// The documentation on the web is outdated.
+            /// The .NET library has been updated to no longer allow generic typing. 
+            /// You must use either Dictionary(String,Object) - use <> but not allowed in XML comments
+            /// OR a user-defined json serializable class - their documentation does not explain how to do this
+            /// well enough, however so we are sticking with the Generic Collection specified 
+            /// </remarks>>
             SODA.Resource<Dictionary<string, object>> dataset = client.GetResource<Dictionary<string, object>>("fhrw-4uyv");
 
-            ///<remarks>
-            ///This is a test for our user defined serializable class
-            ///It may be implemented in later versions 
-            ///</remarks>
-            //SODA.Resource<Dictionary<string, object>> dataset = client.GetResource<Dictionary<string, object>>("fhrw-4uyv"); //testing our serializable json class
-
-            ///<summary>
+            /// <summary>
             /// instantiate our object and get our query from GetQueryDate()
             /// </summary>
             ManageDB test = new ManageDB();
             SODA.SoqlQuery soql = test.GetQueryDate();
 
-
-            ///<summary>
+            /// <summary>
             /// Query sends our query to our pre-defined location and returns an IEnumerable which we assign to results
             /// results now contains the results of our query
-            /// 
-            /// This field for now remains dynamically typed in case we decide to change to our user defined class
             /// </summary>
-            var results = dataset.Query<Dictionary<string, object>>(soql);
+            IEnumerable<Dictionary<string, object>> results = dataset.Query<Dictionary<string, object>>(soql);
 
-
-
-            ///<summary>
+            /// <summary>
             /// Testing to make sure that our query returned results
-            ///</summary>
+            /// Will be changed to throw an error instead of printing a value 
+            /// </summary>
             int SizeOfList;
             test.TestIEnum(ref results, out SizeOfList);
             Console.WriteLine(SizeOfList);
-
-
             return results;
         }
-
-
-
 
         /// <summary>
         /// This function was written to increase readability of the code
@@ -109,12 +158,13 @@ namespace ConsoleApp1
         /// <returns>our Query in their defined type - SODA.SoqlQuery </returns>
         public SODA.SoqlQuery GetQueryDate()
         {
-            
-            DateTime today = DateTime.Today;
+            /// <remarks>
+            /// Get Date and Time of system now so we do not have to keep changing the code
+            /// </remarks>
+            DateTime today = DateTime.Now;
             String year = today.Year.ToString();
 
-
-            ///<remarks>
+            /// <remarks>
             /// Their query requires typing with a 0 in front of the month so we 
             /// check the formatting and add a 0 if necessary
             /// </remarks>
@@ -127,47 +177,54 @@ namespace ConsoleApp1
             {
                 month = "0" + today.Month.ToString();
             }
-            String day = today.Day.ToString();
-            String yday = (today.Day - 1).ToString();
-        
 
-            ///<remarks>
+            /// <remarks>
+            /// Since the data is only updated every day for the day before in part and fully for 
+            /// 2 days before we get the dates for yesterday and the day before for filtering
+            /// </remarks>
+            String day = (today.Day - 1).ToString();
+            String yday = (today.Day - 2).ToString();
+
+            /// <remarks>
             ///The date field in the Query needs to be of type
             ///yyyy-mm-dd + T + hh:mm:ss.nnn (hours, minutes, seconds and nanoseconds.
             ///We format the data here to be in the correct type as the ToString method provided by 
             ///the DateTime class does not allow for formatting in this manner
             /// </remarks>
-            String date= "\"" + year + "-" + month + "-" + day + "T00:00:00.000\"";
+            String date = "\"" + year + "-" + month + "-" + day + "T00:00:00.000\"";
             String ydate = "\"" + year + "-" + month + "-" + yday + "T00:00:00.000\"";
- 
-            ///<remarks>
-            /// just a quick test to make sure the date is in the format we want
-            /// </remarks>
-            //Console.WriteLine(date);
 
-            ///<remarks>
+            /// <remarks>
             /// A test case just to test that the connection is working
             /// will be removed in later revisions
             /// </remarks>
             //SODA.SoqlQuery soql = new SoqlQuery().Select("*").Limit(10);
 
-           ///<remarks>
-           /// for increased readability and easier formatting
-           /// </remarks>
+            /// <remarks>
+            /// for increased readability and easier formatting
+            /// </remarks>
             string cdate = "created_date";
 
-            ///<remarks>
+            /// <remarks>
             /// Usage of creating a new SoqlQuery - 
             /// Note: TimeStamp (or other literal) must be in quotes or else it gets treated like a variable which will throw an error
             /// Field + "function" + "comparison type"
             /// Select can be used to select specific fields but we want all data associated with the calls
+            /// Hour compensates for my estimated time the database is updated with new info
+            /// Will be updated again when we find out when exactly the data is updated every day
             /// </remarks>
-            SODA.SoqlQuery soql = new SoqlQuery().Select("*").Where(cdate + ">" + ydate);
-
+            SODA.SoqlQuery soql;
+            if (today.Hour > 19)
+            {
+                soql = new SoqlQuery().Select("*").Where(cdate + ">" + date);
+            }
+            else
+            {
+                soql = new SoqlQuery().Select("*").Where(cdate + ">" + ydate);
+            }
 
             return soql;
         }
-        
 
         /// <summary>
         /// Internal Test to make sure the query is returning data and not an empty set
@@ -180,55 +237,9 @@ namespace ConsoleApp1
         }
     }
 
-    
-     /// <summary>
-     /// Template for our seriablizable class to potentially replace Dictionary(string, object) (same <> notation, see reason above)
-     /// Will be removed if Dictionary proves to be more useful and useable
-     /// </summary>
-    class Json311
-    {
-        public string unique_key { get; set; }
-        public string created_date { get; set; }
-        public string closed_date { get; set;}
-        public string agency { get; set; }
-        public string complaint_type { get; set; }
-        public string descriptor { get; set; }
-        public string location_type { get; set; }
-        public string incident_zip { get; set; }
-        public string incident_address { get; set; }
-        public string street_name { get; set; }
-        public string cross_street_1 { get; set; }
-        public string cross_street_2 { get; set; }
-        public string intersection_street_1 { get; set; }
-        public string intersection_street_2 { get; set; }
-        public string address_type { get; set; }
-        public string city { get; set; }
-        public string landmark { get; set; }
-        public string facility_type { get; set; }
-        public string status { get; set; }
-        public string due_date { get; set; }
-        public string resolution_description { get; set; }
-        public string resoltuion_action_updated_date { get; set; }
-        public string community_board { get; set; }
-        public string bbl { get; set; }
-        public string borough { get; set; }
-        public string x_coordinate_state_plane { get; set; }
-        public string y_coordinate_state_plane { get; set; }
-        public string open_data_channel_type { get; set; }
-        public string park_facility_name { get; set; }
-        public string park_borough { get; set; }
-        public string vehicle_type { get; set; }
-        public string taxi_company_borough { get; set; }
-        public string taxi_pick_up_location { get; set; }
-        public string bridge_highway_name { get; set; }
-        public string bridge_highway_direction { get; set; }
-        public string road_ramp { get; set; }
-        public string bridge_highway_segment { get; set; }
-        public string latitude { get; set; }
-        public string longitude { get; set; }
-        public string location_city { get; set; }
-        public string location { get; set; }
-        public string location_zip { get; set; }
-        public string location_state { get; set; }
-    }
 }
+
+
+
+
+    
